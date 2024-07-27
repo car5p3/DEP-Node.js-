a62d1ab51842f8318f55e5e674dbd5bab82b9b90
Abdul Rehman Vaqar <143547251+car5p3@users.noreply.github.com>
2024-07-27 20:07:40 +0500

    Task # 04 (Uploaded)
    This task is same as Task # 03.

---
+++ Task # 04/user-service/src/models/User.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+});
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+sequelize.sync();
+
+module.exports = User;
